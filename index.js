88d8d18425d8021cdb990d42eeef5c00f51897a6
victorb <hi.victorb@gmail.com>
2023-01-18 17:53:27 +0000

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const cors = require('cors')();
+const cors = require('cors');
+
+app.use(cors());
 
 let users = [];
 
